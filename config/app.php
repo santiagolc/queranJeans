d22df3e516c9277157826789ff1e8b38b53a1f66
Diego <diedemiguel@gmail.com>
2020-01-27 16:47:30 -0300

    cambios en recuperar contrasenia

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://www.queranjeans.dhalumnos.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
